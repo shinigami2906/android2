7c02fe5b03236f43a1cce2a81a46b5fa0a27c2bc
shini <tuan29061998@gmail.com>
2020-06-05 22:25:40 +0700

    test repo

--- app/src/main/java/vn/daikon/myapplication/MainActivity.java
+++ app/src/main/java/vn/daikon/myapplication/MainActivity.java
@@ -2,42 +2,65 @@ package vn.daikon.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.concurrent.ExecutionException;
 
 import vn.daikon.myapplication.model.textrequest.TextRequest;
 import vn.daikon.myapplication.model.textresponse.TextResponse;
+import vn.daikon.myapplication.repository.LocalRepo;
 import vn.daikon.myapplication.repository.TextApi;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
-    TextView textView;
-
+    EditText et_from;
+    TextView tv_to;
+    TextView tv_list;
+    SharedPreferences sharedPreferences;
+    LocalRepo localRepo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.daikon);
+        et_from = (EditText) findViewById(R.id.from);
+        tv_to = (TextView) findViewById(R.id.to);
+        tv_list = (TextView) findViewById(R.id.list);
+        sharedPreferences = getSharedPreferences("Tuan",MODE_PRIVATE);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                Log.d(TAG,"tuan");
+                localRepo = new LocalRepo(sharedPreferences);
+                String x = localRepo.getTop(1)[0];
+                tv_list.setText(x);
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         TextApi textApi = new TextApi();
-        try {
-            TextResponse[] textResponses = textApi.execute(new TextRequest("chair")).get();
-            if (textResponses != null) {
-                textView.setText(textResponses[0].translation[0].text);
+        String from = et_from.getText().toString();
+        if (from != null && from !="") {
+            try {
+                TextResponse[] textResponses = textApi.execute(new TextRequest(from)).get();
+                if (textResponses != null) {
+                    tv_to.setText(textResponses[0].translation[0].text);
+                    localRepo = new LocalRepo(sharedPreferences);
+                    localRepo.update(from,textResponses[0].translation[0].text);
+                }
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-
-
-
     }
+
+
 }
--- app/src/main/java/vn/daikon/myapplication/repository/LocalRepo.java
+++ app/src/main/java/vn/daikon/myapplication/repository/LocalRepo.java
@@ -2,6 +2,7 @@ package vn.daikon.myapplication.repository;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import java.util.prefs.Preferences;
 
@@ -12,21 +13,26 @@ public class LocalRepo {
     }
 
     public int getCount() {
-        int count = sharedPreferences.getInt("daumanhtuan.count", 0);
+        Integer count = 0 ;
+        if (sharedPreferences.contains("daumanhtuan.count")){
+            count = sharedPreferences.getInt("daumanhtuan.count", 0);
+        }
+        Log.d("phong", Integer.toString(count));
         return count;
     }
     public String[] getTop(int top){
 
         int count = getCount();
         String[] list = new String[Math.min(top,count)];
-        for (int i = 0;i< Math.min(top, count); i++){
-            list[i] = sharedPreferences.getString("daumanhtuan."+ Integer.toString(i), "");
+        for (int i = count; i > count - Math.min(top,count); i--){
+            list[count - i ] = sharedPreferences.getString("daumanhtuan."+ Integer.toString(i), "");
         }
         return list;
     }
     public void update(String from , String to){
         int count = getCount();
+
         sharedPreferences.edit().putString(from, to).putString("daumanhtuan."+ Integer.toString(count+1),from)
-                .putString("daumanhtuan.count", Integer.toString(count+1)).apply();
+                .putInt("daumanhtuan.count", count+1).apply();
     }
 }
