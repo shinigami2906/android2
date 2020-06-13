2739ccdf59378e9e53d125d03c788ee919fe8cfb
shini <tuan29061998@gmail.com>
2020-06-13 16:50:51 +0700

    suw agiao dien

--- app/src/main/java/vn/daikon/myapplication/MainActivity.java
+++ app/src/main/java/vn/daikon/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.util.Pair;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -28,15 +30,15 @@ import vn.daikon.myapplication.presenter.Presenter;
 import vn.daikon.myapplication.repository.LocalRepo;
 import vn.daikon.myapplication.repository.TextApi;
 
-public class MainActivity extends AppCompatActivity implements MyInterface.View {
+public class MainActivity extends AppCompatActivity implements MyInterface.View , AdapterView.OnItemSelectedListener {
 
     Spinner sp_from,sp_to;
-    ImageView im_sw,im_clear;
+    Button im_sw,im_clear;
     TextView tv_out;
     EditText et_inp;
     ListView lv;
     List<Translation> list ;
-    String from, to ;
+    String from = "en", to = "vi";
     LocalRepo localRepo;
     MyInterface.IPresenter presenter;
     @Override
@@ -47,16 +49,29 @@ public class MainActivity extends AppCompatActivity implements MyInterface.View 
         sp_to = (Spinner) findViewById(R.id.spinner_language_to);
         et_inp = (EditText) findViewById(R.id.text_input);
         tv_out = (TextView) findViewById(R.id.text_ouput);
-        im_sw = (ImageView) findViewById(R.id.image_swap);
-        im_clear = (ImageView) findViewById(R.id.clear_text);
+        im_clear = (Button) findViewById(R.id.clear_text);
         lv = (ListView) findViewById(R.id.list_item);
         localRepo = new LocalRepo(this);
         setAdapter();
         presenter = new Presenter(this,localRepo);
+        sp_from.setOnItemSelectedListener(this);
+        sp_to.setOnItemSelectedListener(this);
 
 
     }
+    public void onClickClearButton(View view){
+        tv_out.setText("");
+        et_inp.setText("");
+    }
     void setAdapter(){
+        List<String> list2 = new ArrayList<>();
+        list2.add("Vietnamese");list2.add("English");
+        ArrayAdapter<String> adapterFrom  = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,list2);
+        adapterFrom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_from.setAdapter(adapterFrom);
+        sp_to.setAdapter(adapterFrom);
+        sp_from.setSelection(1);
+        sp_to.setSelection(0);
         list = localRepo.getAllTranslation();
         TranslateAdapter translateAdapter = new TranslateAdapter(list);
         lv.setAdapter(translateAdapter);
@@ -68,9 +83,24 @@ public class MainActivity extends AppCompatActivity implements MyInterface.View 
         tv_out.setText(to);
         list.add(translation);
     }
-    public void onClickTransImage(View view){
+    public void onClickTransButton(View view){
         String inp = et_inp.getText().toString();
 
-        presenter.translate(inp,"en","vi");
+        presenter.translate(inp,from,to);
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String x = "vi";
+        if (position == 0) x = "vi";else x = "en";
+        if (view.getId() == R.id.spinner_language_from) from = x; else to = x;
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
